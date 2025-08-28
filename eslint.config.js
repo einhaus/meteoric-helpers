266f3b9213a0b5010c9f61f4da14179eee88cce8
einhaus <einhaus@gmail.com>
2025-08-28 13:57:00 -0400

    Removed 'sonarjs/no-one-iteration-loop' rule from eslint.config.js and updated package version to 1.0.82 in package.json for improved package management.

--- eslint.config.js
+++ eslint.config.js
@@ -66,7 +66,6 @@ export default [
             'sonarjs/no-identical-conditions': 'error',
             'sonarjs/no-identical-expressions': 'error',
             'sonarjs/no-ignored-return': 'error',
-            'sonarjs/no-one-iteration-loop': 'error',
             'sonarjs/no-use-of-empty-return-value': 'error',
             'sonarjs/non-existent-operator': 'error',
             'for-direction': 'error',
